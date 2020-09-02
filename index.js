e68e9e8caacddf1445c4700614366dd8badc6ea7
Sofia Liendro <sofialiendro.s@gmail.com>
2020-09-02 15:21:55 -0300

    Agrego JS

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
 
-// selecciono los elementos que necesito
+// // selecciono los elementos que necesito
 const form = document.querySelector("form")
-const inputPassword = document.querySelector("#passw")
-const inputEdad = document.querySelector("#age")
+// const inputPassword = document.querySelector("#passw")
+// const inputEdad = document.querySelector("#age")
 
-const checkEdad = document.querySelector("#check-edad")
-const checkPassword = document.querySelector("#check-passw")
+// const checkEdad = document.querySelector("#check-edad")
+// const checkPassword = document.querySelector("#check-passw")
 
-// chequear datos en el submit del form
+// // chequear datos en el submit del form
 
-form.onsubmit = (e) => {
-  e.preventDefault()
-  if (inputEdad.value < 18) {
-    checkEdad.textContent = "Sos menor de edad!"
-    inputEdad.classList.add("input-error")
+// form.onsubmit = (e) => {
+//   e.preventDefault()
+//   if (inputEdad.value < 18) {
+//     checkEdad.textContent = "Sos menor de edad!"
+//     inputEdad.classList.add("input-error")
+//   }
+// }
+
+// // envitar q usuario envie el form, pero que JS tenga acceso a la info //
+const inputNombre = document.querySelector("#name")
+const errorNombre = document.querySelector("#error-nombre")
+
+// form.onsubmit = (e) => {
+//   e.preventDefault();
+
+//   console.log(inputNombre.value)
+
+//   if (inputNombre.value-length < 5) {
+//     console.log("nombre muy corto") // una opcion
+//     errorNombre.textContent = "nombre muy corto." // otra opcion
+//   }
+
+
+// }
+
+// // chequear datos en el blur del input 
+
+// inputPassword.onblur = (e) => {
+//   if (inputPassword.value.length < 8) {
+//     checkPassword.textContent = "Tu password es muy corto!"
+//     inputPassword.classList.add("input-error")
+//   }
+// }
+
+
+inputNombre.onblur = () => {
+  if (inputNombre.value.length < 4) {
+    errorNombre.textContent = "el nombre en muy corto"
+    inputNombre.classList.add("input-error")
   }
 }
 
-// chequear datos en el blur del input 
 
-inputPassword.onblur = (e) => {
-  if (inputPassword.value.length < 8) {
-    checkPassword.textContent = "Tu password es muy corto!"
-    inputPassword.classList.add("input-error")
+// alerta de que se envio el formulario //
+
+form.onsubmit = () => {
+  alert("Se envió el formulario")
+}
+
+
+// para ver que checkeo el usuario en la checkbox
+
+form.onsubmit = (e) => {
+  e.preventDefault(); // esto evita q se mande el form
+
+  const mascotas = document.querySelector("#pets")
+
+  console.log(pets.checked)
+
+  if (pets.checked) {
+    errorNombre.textContent = "Error, elegi uno"
+  }
+
+  else {
+    form.submit()  // para enviarlo
   }
 }
